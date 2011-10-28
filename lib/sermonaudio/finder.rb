d09f965e76e970bff52c080d31971139a3a9e0f2
Matt Bridges <mbridges.91@gmail.com>
2011-10-28 00:38:34 -0500

    Re-organizaed and fine tuned some finder tests

--- lib/sermonaudio/finder.rb
+++ lib/sermonaudio/finder.rb
@@ -8,10 +8,11 @@ module Sermonaudio
     end
 
     def self.newest(opts = {})
+      raise MissingOption, "Please provide valid parameters when retrieving newest entries" if opts.empty?
       call = :newest_sermons_by_speaker if opts.has_key? "SpeakerName"
       call = :newest_sermons_by_member_id if opts.has_key? "MemberID"
-      if opts.has_key? :what && opts[:what] == "series"
-        raise MissingOption, "Please provid a member id when searching for a series" unless opts.has_kay? "MemberID"
+      if opts.has_key?(:what) && opts[:what] == "series"
+        raise MissingOption, "Please provid a member id when searching for a series" unless opts.has_key? "MemberID"
         call = :get_newest_series_by_member_id 
       end
       response = self.request(call, opts)
--- spec/sermon_spec.rb
+++ spec/sermon_spec.rb
@@ -10,9 +10,30 @@ describe Sermonaudio::Sermon do
   it "should have a client with the right WDSL" do
   	sermons.savon.wsdl.document.should eq("http://web4.sa-media.com/SASoapAPI/service.asmx?WSDL")
   end
-  it "should find newest sermons by SpeakerName" do
-  	result = sermons.find :newest, "SpeakerName" => "Lyle Batey"
-  	result.should be_a Savon::SOAP::Response
+
+  describe "#find(:newest)" do
+
+    context "success" do
+
+        it "should find newest sermons by SpeakerName" do
+          result = sermons.find :newest, "SpeakerName" => "Lyle Batey"
+          result.should be_a Savon::SOAP::Response
+        end
+
+    end
+    context "failure" do
+
+      it "should raise an error if no arguments are passed" do
+        lambda { sermons.find(:newest) }.should raise_error(Sermonaudio::MissingOption)
+      end
+      it "should raise an error if invalid arguments are passed" do
+        lambda do
+          sermons.find(:newest, :what => "series")
+        end.should raise_error(Sermonaudio::MissingOption)
+      end
+
+    end
+
   end
 
 end
