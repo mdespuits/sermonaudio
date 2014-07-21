4b70ceb43210380bca1a9615b4ea51227085bf60
Matt Bridges <mbridges.91@gmail.com>
2014-07-21 17:55:58 -0500

    Configure VCR for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ require 'bundler/setup'
 
 require 'sermonaudio' # and any other gems you need
 require 'pry'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/cassettes'
+  c.hook_into :webmock # or :fakeweb
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   # some (optional) config here
