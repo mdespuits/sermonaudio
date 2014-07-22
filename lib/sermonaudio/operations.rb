feaffc68db0182935e7dc319d91ea87a43abe862
Matt Bridges <mbridges.91@gmail.com>
2014-07-22 15:29:54 -0500

    rubocop :smile:

--- lib/sermonaudio/operations.rb
+++ lib/sermonaudio/operations.rb
@@ -3,24 +3,23 @@ module SermonAudio
   # retrieve data from sermonaudio.com
   module Operations
     def get_sermon_info(sermon_id)
-      execute_call(__callee__, {
-        'MemberID' => ENV['SERMONAUDIO_MEMBER_ID'],
-        'Password' => ENV['SERMONAUDIO_PASSWORD'],
-        'SermonID' => sermon_id
-      })
+      execute_call(__callee__,
+                   'MemberID' => ENV['SERMONAUDIO_MEMBER_ID'],
+                   'Password' => ENV['SERMONAUDIO_PASSWORD'],
+                   'SermonID' => sermon_id
+      )
     end
 
     def get_speakers_by_member_id(member_id)
-      response = execute_call(__callee__, { "MemberID" => member_id })
+      response = execute_call(__callee__,  'MemberID' => member_id)
       response[:string] || []
     end
 
     private
 
-      def execute_call(operation, opts = {})
-        response = SermonAudio.client.call(operation, message: opts)
-        response.body["#{operation}_response".to_sym]["#{operation}_result".to_sym]
-      end
-
+    def execute_call(name, opts = {})
+      response = SermonAudio.client.call(name, message: opts)
+      response.body[:"#{name}_response"][:"#{name}_result"]
+    end
   end
 end
