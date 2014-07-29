1491bc3c4a12d95b7d5f6b73d1ece10ca8ce3d33
Matt Bridges <mbridges.91@gmail.com>
2014-07-29 08:49:33 -0500

    Filter out the spec directory for code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 require 'pry' unless ENV["TRAVIS"]
 require 'vcr'
