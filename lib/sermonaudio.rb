1bfd1bb457b017177d6ee87a6223d9a6f31906d2
Matt Bridges <mbridges.91@gmail.com>
2014-07-29 07:57:22 -0500

    Removing old cruft from the codebase

--- lib/sermonaudio.rb
+++ lib/sermonaudio.rb
@@ -2,14 +2,10 @@ $LOAD_PATH.push File.expand_path('../../lib', __FILE__)
 require 'sermonaudio/version'     # Always require version
 require 'savon'
 
-require 'sermonaudio/exceptions'    # For exceptions
-
 # Load SermonAudio specific files
 require 'sermonaudio/configuration'
 require 'sermonaudio/actions'
 require 'sermonaudio/client'
-require 'sermonaudio/sermon'
-require 'sermonaudio/finder'
 
 module SermonAudio
   extend Actions
